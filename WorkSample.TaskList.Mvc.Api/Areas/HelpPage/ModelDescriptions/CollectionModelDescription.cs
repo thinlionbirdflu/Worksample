1ea94452dda5e65d0d7919a3fd81172362838c44
thinlionbirdflu <jgschultz2@gmail.com>
2018-03-29 09:08:10 -0500

    removing unnecessary code

--- WorkSample.TaskList.Mvc.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace WorkSample.TaskList.Mvc.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- WorkSample.TaskList.Mvc.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WorkSample.TaskList.Mvc.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
